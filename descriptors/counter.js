2174b5f58c27cc3e0b172459b2091f99a6af46ae
dmitriyrotaenko <rotaenko.dmitriy@gmail.com>
2025-01-08 22:06:49 +0300

    Solves Counter task

---
+++ descriptors/counter.js
@@ -0,0 +1,17 @@
+function createCounter() {
+	let value = 0;
+	return Object.defineProperty({}, 'count', {
+		get() {
+			return value++;
+		}
+	});
+}
+
+const counter = createCounter();
+
+console.log(counter.count)
+console.log(counter.count)
+console.log(counter.count)
+console.log(counter.count)
+console.log(counter.count)
+console.log(counter.count) // 5 
