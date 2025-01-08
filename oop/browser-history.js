7aa5f5400bc0c0e205138548e861bb96d9e103f2
dmitriyrotaenko <rotaenko.dmitriy@gmail.com>
2025-01-08 20:07:19 +0300

    Refactor BrowserHistory.visit method

--- oop/browser-history.js
+++ oop/browser-history.js
@@ -1,18 +1,11 @@
 class BrowserHistory {
 	constructor(url) {
-		this.initialURL = url;
-		this.history = [this.initialURL];
+		this.history = [url];
 		this.urlPointer = 0;
-
-		this.wasBacked = false;
 	}
 
 	visit(url) {
-		if(this.wasBacked) {
-			this.history = this.history.slice(0, this.urlPointer + 1);
-			this.wasBacked = false;
-		}
-
+		this.history = this.history.slice(0, this.urlPointer + 1);
 		this.history.push(url);
 		this.urlPointer++;
 
@@ -23,8 +16,6 @@ class BrowserHistory {
 		if(this.urlPointer === 0) {
 			return null;
 		}
-
-		this.wasBacked = true;
 
 		this.urlPointer--;
 		return this.history[this.urlPointer];
@@ -42,13 +33,11 @@ class BrowserHistory {
 
 const history = new BrowserHistory("A");
 
-// visit B; visit C; visit D; back; back; visit E, visit F; back; back
+history.visit('B') 
+history.visit('C') 
+history.visit('D') 
 
-history.visit('B') // 1
-history.visit('C') // 2
-history.visit('D') // 3
-
-history.back() // 'C'
+history.back()
 console.log(history.back()) // 'B'
 
 history.visit('E')
@@ -56,4 +45,4 @@ history.visit('F')
 console.log(history.history)
 
 history.back()
-console.log(history.back());
+console.log(history.back()); // 'B'
