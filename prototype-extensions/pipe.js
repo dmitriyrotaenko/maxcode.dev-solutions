06cfa26d12ca2aabeab6d05c002708a1417f0c4b
dmitriyrotaenko <rotaenko.dmitriy@gmail.com>
2025-01-15 22:08:28 +0300

    Solves Function.prototype.pipe

---
+++ prototype-extensions/pipe.js
@@ -0,0 +1,12 @@
+Function.prototype.pipe = function(fn) {
+  return arg => {
+    return [this, fn].reduceRight((res, fn) => fn(res), arg)
+  }
+};
+
+const double = x => x * 2;
+const cube = x => x ** 3;
+const inc = x => x + 1;
+
+const foo2 = inc.pipe(cube).pipe(double);
+console.log(foo2(2)); // 54
